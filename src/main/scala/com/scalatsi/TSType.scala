b2fa3e684219008da85d45d215711130fd0faa5b
Jan-David Salchow <jdsalchow@gmail.com>
2023-01-14 13:00:39 +0100

    Cross compile to scala by removing functionality

--- src/main/scala/com/scalatsi/TSType.scala
+++ src/main/scala/com/scalatsi/TSType.scala
@@ -39,10 +39,10 @@ object TSType extends DefaultTSTypes {
     * Case class will use [[fromCaseClass]]
     * Sealed traits/classes will use [[fromSealed]]
     */
-  def getOrGenerate[T]: TSType[T] = macro Macros.getImplicitMappingOrGenerateDefault[T, TSType]
+//  def getOrGenerate[T]: TSType[T] = macro Macros.getImplicitMappingOrGenerateDefault[T, TSType]
 
   /** Generate a typescript interface for a case class */
-  def fromCaseClass[T]: TSIType[T] = macro Macros.generateInterfaceFromCaseClass[T, TSType]
+//  def fromCaseClass[T]: TSIType[T] = macro Macros.generateInterfaceFromCaseClass[T, TSType]
 
   /** Generate a Typescript discriminated union from a scala sealed trait
     *
@@ -60,7 +60,7 @@ object TSType extends DefaultTSTypes {
     * `type AorB = A | B`
     * @see [Typescript docs on Discriminated Unions](https://www.typescriptlang.org/docs/handbook/unions-and-intersections.html#discriminating-unions)
     */
-  def fromSealed[T]: TSNamedType[T] = macro Macros.generateUnionFromSealedTrait[T, TSType]
+//  def fromSealed[T]: TSNamedType[T] = macro Macros.generateUnionFromSealedTrait[T, TSType]
 
   /** Uses the typescript type of Target whenever we're looking for the typescript type of Source
     * This will not generate a `type Source = Target` line like alias
@@ -145,7 +145,7 @@ object TSNamedType extends DefaultTSTypes {
     *
     * @see [[TSType.getOrGenerate]]
     */
-  def getOrGenerate[T]: TSNamedType[T] = macro Macros.getImplicitMappingOrGenerateDefault[T, TSNamedType]
+//  def getOrGenerate[T]: TSNamedType[T] = macro Macros.getImplicitMappingOrGenerateDefault[T, TSNamedType]
 
   /** Uses the typescript type of Target whenever we're looking for the typescript type of Source
     * This will not generate a `type Source = Target` line like alias
@@ -175,7 +175,7 @@ object TSIType {
     *
     * @see [[TSType.getOrGenerate]]
     */
-  def getOrGenerate[T]: TSIType[T] = macro Macros.getImplicitInterfaceMappingOrGenerateDefault[T, TSType, TSIType]
+//  def getOrGenerate[T]: TSIType[T] = macro Macros.getImplicitInterfaceMappingOrGenerateDefault[T, TSType, TSIType]
 
   /** Uses the typescript type of Target whenever we're looking for the typescript type of Source
     * This will not generate a `type Source = Target` line like alias
